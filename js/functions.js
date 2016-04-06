b0db20db86459318055521f4fa16b8c55c4bac1a
Jonathan Bossenger <info@psykrotek.co.za>
2016-04-06 16:16:00 +0200

    Removing unnessecary code

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,5 @@
 jQuery(function ($) {
     var overlay_image = $( '.et_pb_image_overlay .overlay' );
-    overlay_image.css('position', 'absolute');
     overlay_image.hover(
         function() {
             $( this ).addClass( "aw-show-overlay" );
